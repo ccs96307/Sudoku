460eaf7abdfc9601c2156c6ad613936a9db701fd
ccs96307 <ccs96307@gmail.com>
2019-10-08 23:31:58 +0800

    Fixed some optimal bug

--- Sudoku.py
+++ Sudoku.py
@@ -3,55 +3,55 @@
 
 class Sudoku:
     def __init__(self, board=None):
-        self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [3, 4, 0, 0, 5, 0, 0, 0, 1],
-                      [6, 0, 5, 0, 2, 0, 4, 3, 0],
-                      [5, 0, 0, 0, 0, 9, 8, 1, 0],
-                      [0, 9, 0, 6, 0, 0, 0, 0, 5],
-                      [4, 0, 0, 0, 0, 1, 2, 7, 0],
-                      [2, 0, 8, 0, 9, 0, 1, 6, 0],
-                      [1, 6, 0, 0, 4, 0, 0, 0, 7],
-                      [0, 0, 4, 0, 0, 0, 0, 0, 0]]
-        if board:
+        if board and len(board) == 9 and len(board[0]) == 9:
             self.board = board
-        self.position_state = [[[n for n in range(1, 10)] for a in range(9)] for b in range(9)]
-        self.set_list = []
-        self.iter_times = 0
-        self.board_num = self.set_num()
+        else:
+            self.board = [[8, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 3, 6, 0, 0, 0, 0, 0],
+                          [0, 7, 0, 0, 9, 0, 2, 0, 0],
+                          [0, 5, 0, 0, 0, 7, 0, 0, 0],
+                          [0, 0, 0, 0, 4, 5, 7, 0, 0],
+                          [0, 0, 0, 1, 0, 0, 0, 3, 0],
+                          [0, 0, 1, 0, 0, 0, 0, 6, 8],
+                          [0, 0, 8, 5, 0, 0, 0, 1, 0],
+                          [0, 9, 0, 0, 0, 0, 4, 0, 0]]
 
-    def board_plot(self):
+        self.positionState = [[[n for n in range(1, 10)] for a in range(9)] for b in range(9)]
+        self.setList = []
+        self.iterTimes = 0
+        self.boardNum = self.setNum()
+
+    def clean(self):
+        None
+
+
+    def plotBoard(self):
         for _ in range(9):
             print(self.board[_])
-        print('-------------------------------------', self.set_num())
+        print('-------------------------------------', self.setNum())
 
-    def only_one_check(self, x, y):
+    def onlyOneCheck(self, x, y):
+        new_x, new_y = x//3*3, y//3*3
         row = self.board[x]
         column = [self.board[_][y] for _ in range(9)]
-        new_x = x//3*3
-        new_y = y//3*3
         square = [self.board[a][b] for a in range(new_x, new_x+3) for b in range(new_y, new_y+3)]
 
-        total_collision_num_list = list(set(row + column + square))
-        total_num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        total_collision_num_list = list(set(row+column+square))
+        total_num_list = [n for n in range(1, 10)]
 
-        for _ in total_collision_num_list:
-            if _ in total_num_list:
-                total_num_list.remove(_)
+        for n in total_collision_num_list:
+            if n in total_num_list:
+                total_num_list.remove(n)
 
-        if len(total_num_list) == 1:
-            return total_num_list[0]
-        else:
-            return False
+        if len(total_num_list) == 1: return total_num_list[0]
+        else: return False
 
     def row_col(self, x, y, n):
-        if n in self.board[x]:
-            return True
+        return n in self.board[x]
 
     def column_col(self, x, y, n):
         column_list = [self.board[_][y] for _ in range(9)]
-
-        if n in column_list:
-            return True
+        return n in column_list
 
     def square_col(self, x, y, n):
         new_x = x//3*3
@@ -61,8 +61,8 @@ class Sudoku:
         if n in square_list:
             return True
 
-    # count the number we set into the board
-    def set_num(self):
+    # Count the number we set into the board
+    def setNum(self):
         num = 0
 
         for a in range(9):
@@ -73,7 +73,7 @@ class Sudoku:
         return num
 
     def board_num_add_check(self, n):
-        board_num_now = self.set_num()
+        board_num_now = self.setNum()
 
         if board_num_now > n:
             return True, board_num_now
@@ -81,11 +81,10 @@ class Sudoku:
             return False, board_num_now
 
     def simplify_sudoku(self, n):
-        self.board_plot()
         for a in range(9):
             for b in range(9):
                 if self.board[a][b] == 0:
-                    result = self.only_one_check(a, b)
+                    result = self.onlyOneCheck(a, b)
 
                     if not result:
                         continue
@@ -95,7 +94,6 @@ class Sudoku:
         test, num = self.board_num_add_check(n)
 
         if test:
-            self.board_plot()
             x, y = self.simplify_sudoku(num)
             return x, y
         else:
@@ -113,14 +111,14 @@ class Sudoku:
 
         while True:
             if self.board[i][j] == 0:
-                if self.position_state[i][j]:
-                    n = self.position_state[i][j].pop()
+                if self.positionState[i][j]:
+                    n = self.positionState[i][j].pop()
                     if self.row_col(i, j, n) or self.column_col(i, j, n) or self.square_col(i, j, n):
                         continue
                     else:
                         self.board[i][j] = n
-                        self.board_plot()
-                        self.set_list.append([i, j])
+                        # self.board_plot()
+                        self.setList.append([i, j])
                         result = self.check_end()
 
                         if not result:
@@ -134,9 +132,8 @@ class Sudoku:
                             break
 
                 else:
-                    self.position_state[i][j] = [_ for _ in range(1, 10)]
-                    i, j = self.set_list.pop()
-                    print(i, j)
+                    self.positionState[i][j] = [_ for _ in range(1, 10)]
+                    i, j = self.setList.pop()
                     self.board[i][j] = 0
 
             else:
@@ -148,10 +145,23 @@ class Sudoku:
 
 
 if __name__ == '__main__':
-    sudoku = Sudoku()
-    i, j = sudoku.simplify_sudoku(sudoku.board_num)
+    question = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [3, 4, 0, 0, 5, 0, 0, 0, 1],
+                [6, 0, 5, 0, 2, 0, 4, 3, 0],
+                [5, 0, 0, 0, 0, 9, 8, 1, 0],
+                [0, 9, 0, 6, 0, 0, 0, 0, 5],
+                [4, 0, 0, 0, 0, 1, 2, 7, 0],
+                [2, 0, 8, 0, 9, 0, 1, 6, 0],
+                [1, 6, 0, 0, 4, 0, 0, 0, 7],
+                [0, 0, 4, 0, 0, 0, 0, 0, 0]]
+
+
+    sudoku = Sudoku(question)
+    i, j = sudoku.simplify_sudoku(sudoku.boardNum)
     result = sudoku.check_end()
     if result:
         print('The End')
     else:
         sudoku.run()
+
+    sudoku.plotBoard()
